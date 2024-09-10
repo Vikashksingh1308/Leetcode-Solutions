319ccf27fe4522a110bc4d47d85c1034788195bb
Vikash Kumar Singh <43766381+Vikashksingh1308@users.noreply.github.com>
2024-09-11 00:24:34 +0100

    Update RepeatedSubstring.java

--- Strings/RepeatedSubstring.java
+++ Strings/RepeatedSubstring.java
@@ -1,3 +1,14 @@
+/**
+ Repeated Substring Pattern
+ Given a string s, check if it can be constructed by taking a substring of it and 
+ appending multiple copies of the substring together.
+
+ Example:
+ Input: s = "abab"
+ Output: true
+ Explanation: It is the substring "ab" twice.
+ */
+
 public class RepeatedSubstring {
     public static void main(String[] args){
     RepeatedSubstringPattern r = new RepeatedSubstringPattern();
