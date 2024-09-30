2a2fef75bb8f37f464f010e98bbb3f8a1ff4d4b0
Vikash <vikashksingh1308@gmail.com>
2024-09-30 20:35:19 +0100

    RobotReturn added

---
+++ Strings/RobotReturn.java
@@ -0,0 +1,25 @@
+public class RobotReturn {
+    public static boolean judgeCircle(String moves) {
+        int updown = 0;
+        int leftright = 0;
+
+        for(char c: moves.toCharArray()){
+            if(c == 'U'){
+                updown = updown + 1;
+            } else if(c == 'D'){
+                updown = updown - 1;
+            } else if(c == 'R'){
+                leftright = leftright + 1;
+            } else {
+                leftright = leftright - 1;
+            }
+        }
+
+        return updown == 0 && leftright == 0;
+    }
+
+    public static void main(String[] args) {
+        String moves = "UDUDUDLLRR";
+        System.out.println(judgeCircle(moves));
+    }
+}
